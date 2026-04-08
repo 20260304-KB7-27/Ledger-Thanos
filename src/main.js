028de3c16233db92b34e7ea5850782b7432bf012
Mingyu Joung <samu9nai@gmail.com>
2026-04-08 11:11:17 +0900

    delete: BoxTestApp.vue 삭제
    테스트용으로 사용한 BoxTestApp.vue 삭제

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
-// import App from './BoxTestApp.vue'; // Box 컴포넌트 테스트 시 활성화
 import router from './router';
 
 const app = createApp(App);
