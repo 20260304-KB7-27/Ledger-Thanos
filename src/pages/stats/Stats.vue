7fb2509022946a0cca841db2d775584011d0b98c
HwangSeo <sadclara0830@naver.com>
2026-04-08 15:56:49 +0900

    feat : 헤더 UI 구현

--- src/pages/stats/Stats.vue
+++ src/pages/stats/Stats.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>통계 페이지</div>
+  <div class="main">
+    <div class="header">
+      <Box width="custom" custom-width="15%">
+        <div class="box-label">주간/월간</div>
+      </Box>
+      <h3><span><</span> 2024년 8월 <span>></span></h3>
+      <Box width="custom" customWidth="15%">
+        <div class="box-label">PDF 내보내기</div>
+      </Box>
+    </div>
+    <div>
+      
+    </div>
+  </div>
 </template>
 
 <script setup>
-
-
-
+import Box from '@/components/Box.vue';
 </script>
 
 <style scoped>
-
-
-
+.main {
+  padding: 30px 30px 0px 30px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.box-label {
+  width: 100%;
+  text-align: center;
+}
 </style>
